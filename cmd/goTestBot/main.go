01d676467fbf6a22ce75bd460c41743f602af4e6
Denis Kosenkov <wdenkosw@gmail.com>
2021-10-16 13:08:22 +0300

    gitignore

--- cmd/goTestBot/main.go
+++ cmd/goTestBot/main.go
@@ -1,18 +1,19 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+	"github.com/joho/godotenv"
 )
 
 const tokenVariable = "TG_TEST_TOKEN"
 
 func main() {
+	godotenv.Load()
+
 	token := os.Getenv(tokenVariable)
-	fmt.Printf(token)
 
 	bot, err := tgbotapi.NewBotAPI(token)
 	if err != nil {
