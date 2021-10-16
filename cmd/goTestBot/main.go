536101098ef0133dd967053aa14cd00e29dfe4f7
Denis Kosenkov <wdenkosw@gmail.com>
2021-10-16 13:00:57 +0300

    MVP bot

--- cmd/goTestBot/main.go
+++ cmd/goTestBot/main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+)
 
 func main() {
-	fmt.Printf("Hello.")
+	bot, err := tgbotapi.NewBotAPI("2090959274:AAH7VhBQVn3NAPbbT_utQQxbWaGlmP0dMT0")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.UpdateConfig{
+		Timeout: 60,
+		Offset:  0,
+		Limit:   0,
+	}
+
+	updates, err := bot.GetUpdatesChan(u)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+
+		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "You wrote: \""+update.Message.Text+"\"")
+		msg.ReplyToMessageID = update.Message.MessageID
+
+		bot.Send(msg)
+	}
 }
